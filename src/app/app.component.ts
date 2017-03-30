0f4199015837df2b53c6f930664b39a39fbe3337
Max Sheikhizadeh <max@Maxs-MacBook-Pro.local>
2017-03-30 02:09:12 -0700

    removed sass component + completed the app component with new if else and more

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
+console.log('It works here')
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.sass']
 })
+
 export class AppComponent {
-  title = 'app works!';
+  title: any = true;
+
+  logOut() {
+    console.log('Hello world from logOut fn!');
+    this.title = !this.title;
+  }
+
+  greeter(person: string) {
+    return "Hello, " + person;
+  }
+
+  // validator
+
 }
